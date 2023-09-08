6cc2d483fab72cdaad955e88df42eb1728d87fd9
jv-mendoza <jv.mendoza@genies.com>
2023-09-08 13:42:17 -0700

    add exit code for execute, clean up logs

--- Assets/Editor/CustomBuildScript.cs
+++ Assets/Editor/CustomBuildScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
@@ -17,20 +18,20 @@ namespace Editor
         /// </summary>
         public static void Execute()
         {
+            // dont show stacktrace for normal logs
+            Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
+            
             try
             {
                 var profile = GetCommandArg("-profile") ?? "default";
                 var platform = GetCommandArg("-platform") ?? EditorUserBuildSettings.activeBuildTarget.ToString();
-                
-                DoPreProcessing(profile, () =>
-                {
-                    DoBuilding(profile);
-                });
+
+                DoPreProcessing(profile, () => { DoBuilding(profile); });
             }
             catch (Exception ex)
             {
                 Debug.LogException(ex);
-                
+
                 if (Application.isBatchMode)
                     EditorApplication.Exit(1);
             }
@@ -43,16 +44,26 @@ namespace Editor
             await Task.Delay(7777);
             StopWatch.Stop();
             Debug.Log($"Finished DoPreProcessing [Duration: {StopWatch.Elapsed:g}]");
-            
+
             onPreProcessComplete?.Invoke();
         }
 
         private static async void DoBuilding(string profile)
         {
-            StopWatch.Restart();
-            await Task.Delay(5555);
-            StopWatch.Stop();
-            Debug.Log($"Finished DoBuilding [Duration: {StopWatch.Elapsed:g}]");
+            try
+            {
+                StopWatch.Restart();
+                await Task.Delay(5555);
+                StopWatch.Stop();
+                Debug.Log($"Finished DoBuilding [Duration: {StopWatch.Elapsed:g}]");
+
+                if (Application.isBatchMode) EditorApplication.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                if (Application.isBatchMode) EditorApplication.Exit(1);
+            }
         }
 
         private static string GetCommandArg(string name)
